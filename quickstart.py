3090b8b1334a29cdfc7c2176f43e88c179de6f54
Nilakshi23 <84282600+Nilakshi23@users.noreply.github.com>
2022-02-10 01:33:43 +0530

    Update quickstart.py

--- quickstart.py
+++ quickstart.py
@@ -6,18 +6,18 @@ from httplib2 import Http
 def main():
     """Hangouts Chat incoming webhook quickstart."""
     url = 'https://chat.googleapis.com/v1/spaces/AAAARM7dFAE/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=73W5Cltjjjv3IGdUv0MWoMP8eegMavqPu3HpAYTeL5I%3D'
-    bot_message = {
-        'text' : ' Build is - ${env.BUILD_NUMBER} ,${env.JOB_NAME}'}
-
+    #bot_message = {
+        #'text' : ' Build is - ${env.BUILD_NUMBER} ,${env.JOB_NAME}'}
+    message = "BUILD_NUMBER - %d\n %s" %(${BUILD_NUMBER},status)
     message_headers = {'Content-Type': 'application/json; charset=UTF-8'}
-
+    
     http_obj = Http()
 
     response = http_obj.request(
         uri=url,
         method='POST',
         headers=message_headers,
-        body=dumps(bot_message),
+        #body=dumps(bot_message),
     )
 
     print(response)
