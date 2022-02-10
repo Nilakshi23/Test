06c82f81798fcf55052ee7550c191b0f8c3a5340
Nilakshi23 <84282600+Nilakshi23@users.noreply.github.com>
2022-02-10 10:02:50 +0530

    Update quickstart.py

--- quickstart.py
+++ quickstart.py
@@ -19,20 +19,20 @@ get_job_details()
 def chatbot() :
 	from json import dumps
 
-from httplib2 import Http
+	from httplib2 import Http
 
 
-def main():
-    """Hangouts Chat incoming webhook quickstart."""
-    url = 'https://chat.googleapis.com/v1/spaces/AAAARM7dFAE/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=73W5Cltjjjv3IGdUv0MWoMP8eegMavqPu3HpAYTeL5I%3D'
-    bot_message = {
-        'text' : "Job_name %s" %(job_instance.name) }
+	def main():
+    	"""Hangouts Chat incoming webhook quickstart."""
+    	url = 'https://chat.googleapis.com/v1/spaces/AAAARM7dFAE/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=73W5Cltjjjv3IGdUv0MWoMP8eegMavqPu3HpAYTeL5I%3D'
+    	bot_message = {
+        	'text' : "Job_name %s" %(job_instance.name) }
 
-    message_headers = {'Content-Type': 'application/json; charset=UTF-8'}
+   	 message_headers = {'Content-Type': 'application/json; charset=UTF-8'}
 
-    http_obj = Http()
+    	http_obj = Http()
 
-    response = http_obj.request(
+    	response = http_obj.request(
         uri=url,
         method='POST',
         headers=message_headers,
