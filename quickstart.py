cc30be510bc3e615f2a89288ab2383e035b828b9
Nilakshi23 <84282600+Nilakshi23@users.noreply.github.com>
2022-02-10 09:53:23 +0530

    Update quickstart.py

--- quickstart.py
+++ quickstart.py
@@ -1,4 +1,23 @@
-from json import dumps
+from jenkinsapi.jenkins import Jenkins
+
+def get_server_instance():
+    jenkins_url = 'http://localhost:8080/'
+    server = Jenkins(jenkins_url, username='nilaxshi', password='1234')
+    return server
+    
+def get_job_details():
+    # Refer Example #1 for definition of function 'get_server_instance'
+    server = get_server_instance()
+    for job_name, job_instance in server.get_jobs():
+        print('Job Name:%s' %(job_instance.name))
+        print('Job Description:%s' %(job_instance.get_description()))
+        print('Is Job running:%s' %(job_instance.is_running()))
+        print('Is Job enabled:%s' %(job_instance.is_enabled()))
+
+get_job_details()
+
+def chatbot() :
+	from json import dumps
 
 from httplib2 import Http
 
@@ -6,21 +25,23 @@ from httplib2 import Http
 def main():
     """Hangouts Chat incoming webhook quickstart."""
     url = 'https://chat.googleapis.com/v1/spaces/AAAARM7dFAE/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=73W5Cltjjjv3IGdUv0MWoMP8eegMavqPu3HpAYTeL5I%3D'
-    #bot_message = {
-        #'text' : ' Build is - ${env.BUILD_NUMBER} ,${env.JOB_NAME}'}
-    message = { 'text' : "BUILD_NUMBER is ${BUILD_NUMBER}" }
+    bot_message = {
+        'text' : "Job_name %s" %(job_instance.name) }
+
     message_headers = {'Content-Type': 'application/json; charset=UTF-8'}
-    
+
     http_obj = Http()
 
     response = http_obj.request(
         uri=url,
         method='POST',
         headers=message_headers,
-        body=dumps(message),
+        body=dumps(bot_message),
     )
 
     print(response)
 
 if __name__ == '__main__':
     main()
+	
+chatbot()
