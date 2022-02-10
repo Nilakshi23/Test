9f13e22d57598dd421147cff003256b60dffe310
Nilakshi23 <84282600+Nilakshi23@users.noreply.github.com>
2022-02-10 10:06:36 +0530

    Update quickstart.py

--- quickstart.py
+++ quickstart.py
@@ -23,25 +23,25 @@ def chatbot() :
 
 
 	def main():
-    	"""Hangouts Chat incoming webhook quickstart."""
-    	url = 'https://chat.googleapis.com/v1/spaces/AAAARM7dFAE/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=73W5Cltjjjv3IGdUv0MWoMP8eegMavqPu3HpAYTeL5I%3D'
-    	bot_message = {
-        	'text' : "Job_name %s" %(job_instance.name) }
+    	
+    		url = 'https://chat.googleapis.com/v1/spaces/AAAARM7dFAE/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=73W5Cltjjjv3IGdUv0MWoMP8eegMavqPu3HpAYTeL5I%3D'
+    		bot_message = {
+        		'text' : "Job_name %s" %(job_instance.name) }
 
-   	 message_headers = {'Content-Type': 'application/json; charset=UTF-8'}
+   	 	message_headers = {'Content-Type': 'application/json; charset=UTF-8'}
 
-    	http_obj = Http()
+    		http_obj = Http()
 
-    	response = http_obj.request(
-        uri=url,
-        method='POST',
-        headers=message_headers,
-        body=dumps(bot_message),
-    )
+    		response = http_obj.request(
+       		uri=url,
+        	method='POST',
+        	headers=message_headers,
+        	body=dumps(bot_message),
+    	)
 
-    print(response)
+    	print(response)
 
-if __name__ == '__main__':
-    main()
+	if __name__ == '__main__':
+   		 main()
 	
 chatbot()
